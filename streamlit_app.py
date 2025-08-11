0fc5c3530cb7619a6b21393f7222c599ac4f8887
brenaldo19 <breno.alkmin@gmail.com>
2025-08-11 14:21:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from src.dicionario import dic
 # Ative só se o arquivo existir:
 # from src.ui.autotestes import *
 
-from src.utils import normalizar, calcular_imc, classificar_imc
+from src.utils import calcular_imc, classificar_imc
 from src.dados_sintomas import sistemas_sintomas, sintoma_para_sistema
 from src.logica import (
     gerar_sistemas_afetados_por_fatores,
