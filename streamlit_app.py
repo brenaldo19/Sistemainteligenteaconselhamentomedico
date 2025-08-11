265132e12d8bc961db4f6d37843c5c6ff501cce5
brenaldo19 <breno.alkmin@gmail.com>
2025-08-11 15:14:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,7 +132,6 @@ sistemas_autotestes = {
     "☕ Energia e Vitalidade": ["Energia Matinal", "Variação de Peso (Últimos 30 Dias)"],
     "🩺 Testes de apuração de sintomas específicos": [
         # Alertas/neurológico/obstétrico
-        "Perda Súbita de Coordenação",
         "Desmaio ou tontura",
         "Trabalho de parto",
         "Redução dos movimentos fetais",
@@ -230,7 +229,6 @@ router = {
     "Variação de Peso (Últimos 30 Dias)": render_variacao_peso_30d,
 
     # Específicos
-    "Perda Súbita de Coordenação": render_perda_subita_de_coordenacao,
     "Desmaio ou tontura": render_desmaio_tontura,
     "Trabalho de parto": render_trabalho_de_parto,
     "Redução dos movimentos fetais": render_reducao_movimentos_fetais,
