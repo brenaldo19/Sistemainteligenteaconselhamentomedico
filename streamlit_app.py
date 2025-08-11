db597602a7882cfcc3070c898136287ab4382c81
brenaldo19 <breno.alkmin@gmail.com>
2025-08-11 20:14:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ from pathlib import Path
 import time
 import random
 import pandas as pd
+import importlib
+at = importlib.import_module("src.ui.autotestes")
 
 # ==== PATHS ====
 ROOT = Path(__file__).resolve().parent
